1cf8d4ecf848c6dd3579fcca6e5d0ee55b79c7b5
lucas <lucasnumeriano.dev@gmail.com>
2026-02-06 20:12:18 -0300

    Inital Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        neon: '#00ffcc',
+        panel: '#0b0f14',
+      },
+    },
+  },
+  plugins: [],
+};
